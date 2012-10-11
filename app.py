ac4e9bdbe9b787aabfacbfb39a906e0744f63eae
Robert Rees <robert.rees@guardian.co.uk>
2012-10-11 18:31:28 +0100

    Creates a working HTML page

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import webapp2
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      self.response.write('Hello, webapp2 World!')
+	def get(self):
+		template = jinja_environment.get_template('index.html')
+		
+		template_values = {}
+
+		self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/', MainPage)],
                               debug=True)
